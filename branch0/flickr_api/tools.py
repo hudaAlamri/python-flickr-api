11a8751f67690e90d9102c35c0e2d662ccd8b02b
alexis.mignon <alexis.mignon@c04dc755-9e73-fb1f-9025-638fad5d94ba>
2012-03-19 13:08:48 +0000

    New automatically generated doc added

--- branch0/flickr_api/tools.py
+++ branch0/flickr_api/tools.py
@@ -1,6 +1,6 @@
-from base import FlickrError,dict_converter
 from method_call import clean_content,call_api
 import method_call
+import sys,os
 
 def load_methods():
     """
@@ -16,10 +16,11 @@ def methods_info():
     for m in load_methods():
         info = call_api(method = "flickr.reflection.getMethodInfo",method_name = m)
         info.pop("stat")
-        conv = dict_converter(["needslogin","needssigning"],bool)
         method = info.pop("method")
         conv(method)
         method["requiredperms"] = __perms__[method["requiredperms"]]
+        method["needslogin"] = bool(method.pop("needslogin"))
+        method["needssigning"] = bool(method.pop("needssigning"))
         info.update(method)
         info["arguments"] = info["arguments"]["argument"]
         info["errors"] = info["errors"]["error"]
@@ -34,3 +35,19 @@ def write_reflection(path,template,methods = None):
     with open(path,"w") as f:
         f.write(templ%str(methods))
 
+def write_doc(output_path,exclude = ["flickr_keys","methods"]):
+    import flickr_api
+    exclude.append("__init__")
+    modules = ['flickr_api']
+    dir = os.path.dirname(flickr_api.__file__)
+    modules += [ "flickr_api."+f[:-3] for f in os.listdir(dir) if f.endswith(".py") and f[:-3] not in exclude]
+    sys.path.insert(0,dir+"../")
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+    os.chdir(output_path)
+    for m in modules :
+        os.system("pydoc -w "+m)
+
+    
+    
+    
